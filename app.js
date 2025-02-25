b75cf2029abbbd840875b88ca967073dc48f6f60
Nagul75 <nagulk697@gmail.com>
2025-02-25 11:50:04 +0530

    Set up Routes and EJS

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const indexRouter = require('./routes/indexRouter')
+const newRouter = require('./routes/newRouter')
+
+const app = express()
+const PORT = 8080
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+const assetsPath = path.join(__dirname, "public")
+app.use(express.static(assetsPath))
+
+app.use("/", indexRouter)
+app.use("/new", newRouter)
+
+app.listen(PORT, () => {
+    console.log(`Listening at Port: ${PORT}`)
+})
